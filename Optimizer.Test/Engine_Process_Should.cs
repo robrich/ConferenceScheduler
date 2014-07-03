1bed22b0647c562cceabd061e705ec50f6bf6755
Barry Stahl <bss_github@bsstahl.com>
2014-07-02 19:57:49 -0700

    New solution with main project and test library

---
+++ Optimizer.Test/Engine_Process_Should.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConferenceScheduler.Optimizer;
+
+namespace ConferenceScheduler.Optimizer.Test
+{
+    [TestFixture]
+    public class Engine_Process_Should
+    {
+        [Test]
+        public void RunSuccessfullyIfNoDataSupplied()
+        {
+            Engine engine = new Engine();
+            engine.Process();
+            // Assert.That(true, Is.EqualTo(true), "It didn't blow up"); // TODO: when Process() returns something build a real assert
+            Assert.True(true, "It didn't blow up");
+        }
+    }
+}
---
+++ Optimizer/Engine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ConferenceScheduler.Optimizer
+{
+    public class Engine
+    {
+        [SuppressMessage("Microsoft.Performance", "CA1822:MarkMembersAsStatic")]
+        public void Process()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
