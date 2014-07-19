328756c4e91a97ebb6d2cbb148d8b2240e44b9d4
Barry Stahl <bss_github@bsstahl.com>
2014-07-19 06:41:30 -0700

    Added additional test for validating tracks

--- Optimizer.Test/Engine_Process_ShouldOptimizeBy.cs
+++ Optimizer.Test/Engine_Process_ShouldOptimizeBy.cs
@@ -62,7 +62,7 @@ namespace ConferenceScheduler.Optimizer.Test
         }
 
         [Test]
-        public void SeparatingSessionsInTheSameTrackIntoDifferentTimslots()
+        public void SeparatingSessionsInTheSameTrackIntoDifferentTimslots_4Sessions3Tracks()
         {
             Engine engine = new Engine();
             var sessions = new SessionsCollection();
@@ -85,6 +85,32 @@ namespace ConferenceScheduler.Optimizer.Test
             var s1TimeslotId = assignments.Where(a => a.SessionId == 1).Single().TimeslotId;
             var s3TimeslotId = assignments.Where(a => a.SessionId == 3).Single().TimeslotId;
             Assert.That(s1TimeslotId, Is.Not.EqualTo(s3TimeslotId), "Sessions with the same TopicId should not be in the same timeslot.");
+        }
+
+        [Test]
+        public void SeparatingSessionsInTheSameTrackIntoDifferentTimslots_4Sessions1Track()
+        {
+            Engine engine = new Engine();
+            var sessions = new SessionsCollection();
+            sessions.Add(1, null, Presenter.Create(1));
+            sessions.Add(2, 1, Presenter.Create(2));
+            sessions.Add(3, null, Presenter.Create(3));
+            sessions.Add(4, 1, Presenter.Create(4));
+
+            var rooms = new List<Room>();
+            rooms.Add(Room.Create(1, 10));
+            rooms.Add(Room.Create(2, 10));
+
+            var timeslots = new List<Timeslot>();
+            timeslots.Add(Timeslot.Create(1, 9.0));
+            timeslots.Add(Timeslot.Create(2, 10.25));
+
+            var assignments = engine.Process(sessions, rooms, timeslots);
+            assignments.WriteSchedule();
+
+            var s2TimeslotId = assignments.Where(a => a.SessionId == 2).Single().TimeslotId;
+            var s4TimeslotId = assignments.Where(a => a.SessionId == 4).Single().TimeslotId;
+            Assert.That(s2TimeslotId, Is.Not.EqualTo(s4TimeslotId), "Sessions with the same TopicId should not be in the same timeslot.");
         }
 
     }
